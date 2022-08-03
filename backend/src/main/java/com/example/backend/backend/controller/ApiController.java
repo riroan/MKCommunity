7d2124cb8abf78c6604edd40bd89cb7c54bb01d1
riroan <riroan@naver.com>
2022-08-04 06:07:40 +0900

    feat: 데이터 송수신 추가

--- backend/src/main/java/com/example/backend/backend/controller/ApiController.java
+++ backend/src/main/java/com/example/backend/backend/controller/ApiController.java
@@ -1,26 +1,39 @@
 package com.example.backend.backend.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend.backend.domain.Message;
 import com.example.backend.backend.dto.MessageDTO;
+import com.example.backend.backend.repository.MessageRepository;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    @GetMapping("")
-    public String hello() {
-        return "Hello";
-    }
+    @Autowired
+    MessageRepository messageRepository;
     
     @PostMapping("/write")
     public MessageDTO write(@RequestBody MessageDTO message) {
-        System.out.println(message);
+        Message msg = new Message();
+        msg.setTitle(message.getTitle());
+        msg.setContent(message.getContent());
+        msg.setCreatedAt(LocalDateTime.now());
+        messageRepository.save(msg);
         return message;
     }
 
+    @GetMapping("/msg")
+    public List<Message> get() {
+        List<Message> result = messageRepository.findAll();
+        return result;
+    }
 }
 
